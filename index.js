0efcf12f2a0d6c6891b71d552900c3d9e678b6e2
Yousef. <kingyousef922@gmail.com>
2023-10-01 14:26:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, gift) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${gift} gift!`;
+        messages.push(message);
+    }
+    return messages;
+
+}
+function countDown(numberr) {
+    let countdown = numberr;
+    while (countdown >= 0) {
+        console.log(countdown);
+        countdown--
+    }
+}
